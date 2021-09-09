fb8dc3b779a1c0db2f5b49ec43bef47bd2c8f4b4
SteamDwarf <schajckodmitrij@gmail.com>
2021-09-09 16:20:29 +0300

    Homework-2

---
+++ src/components/Layout.js
@@ -0,0 +1,27 @@
+import styles from './Layout.module.css';
+
+const Layout = ({title, desc, urlBg, colorBg}) => {
+    const inlineStyles = {
+        backgroundImage: `url(${urlBg})`,
+        backgroundColor: colorBg
+    }
+    console.log(inlineStyles);
+
+    return (
+        <section className={styles.root} style={inlineStyles}>
+            <div className={styles.wrapper}>
+                <article>
+                    <div className={styles.title}>
+                        <h3>{title}</h3>
+                        <span className={styles.separator}></span>
+                    </div>
+                    <div className={styles.desc, styles.full}>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Layout;
